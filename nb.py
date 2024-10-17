a69d3cbf271284cb0968938596a3a7c8b62c8444
Hendrik Unger <unger.hendrik@gmail.com>
2024-10-17 16:05:02 +0200

    Updates to opc server

--- nb.py
+++ nb.py
@@ -12,46 +12,51 @@ namespace = "urn:WIN-IBKT0GFPRI7:XdcOpcua"
 transcation_types = ["SER", "BOX", "DAT", "PAC", "HMIAddKomp", "HMIData", "TRA", "USR", "OEE"]
 
 
-async def main():
+async def list():
 
-    print(f"Connecting to {url} ...")
+    print(f"Connecting to {url} ...", flush=True)
     client = Client(url=url)
     client.set_user("VDC01")
     client.set_password("Vdc01C123")
     client.session_timeout = 100000
     await client.connect()
     nsidx = await client.get_namespace_index(namespace)
-    print(f"Namespace Index for '{namespace}': {nsidx}")
-    mesroot = client.get_node("ns=2;s=C1")
-    print(f"MES Root Adress: {mesroot}")
-    print("")
+    print(f"Namespace Index for '{namespace}': {nsidx}", flush=True)
+    mesroot = client.get_node("ns=2;s=MES Root")
+    print(f"MES Root Adress: {mesroot}", flush=True)
+    print("", flush=True)
     machines = await mesroot.get_children()
     for machine in machines:
         machine_name = (await machine.read_display_name()).Text
-        print(machine_name)
+        print(machine_name, flush=True)
         transactions = await machine.get_children()
         for transaction in transactions:
             transaction_name = (await transaction.read_display_name()).Text
-            print(f"   {transaction_name}")
+            print(f"   {transaction_name}", flush=True)
             if transaction_name in transcation_types:
                 trans_data = await transaction.get_children()
-                for node_data in trans_data:
-                    node_data_name = (await node_data.read_display_name()).Text
-                    node_data_value = await node_data.read_value()
-                    print(f"      {node_data_name}")
-                    if type(node_data_value) == list:
-                        for element in node_data_value:
-                            print(f"         {element}")
-                    else:
-                        print(f"         {node_data_value}")
+                for interface in trans_data:
+                    interface_data = await interface.get_children()
+                    interface_data_name = (await interface.read_display_name()).Text
+                    print(f"      {interface_data_name}", flush=True)
+                    for node_data in interface_data:
+                        node_data_name = (await node_data.read_display_name()).Text
+                        #node_data_value = await node_data.read_value()
+                        print(f"        {node_data_name}", flush=True)
+                        # if type(node_data_value) == list:
+                        #     for element in node_data_value:
+                        #         print(f"         {element}", flush=True))
+                        # else:
+                        #     print(f"         {node_data_value}", flush=True))
 
 
     await client.disconnect()
 
 
 
-await main()
 
+if __name__ == "__main__":
+    asyncio.run(list())
 
 
 # %%
